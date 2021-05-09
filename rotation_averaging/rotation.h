1cec548c4668a851d2f83634dafa796466fce49d
junlinp <junlinp@qq.com>
2021-05-09 20:52:42 +0800

    Update rotation.h

--- rotation_averaging/rotation.h
+++ rotation_averaging/rotation.h
@@ -8,6 +8,12 @@ void quaternion_to_angle_axis(const T*quaternion, T* angle_aixs) {
     angle_aixs[0] = quaternion[1] * factor;
     angle_aixs[1] = quaternion[2] * factor;
     angle_aixs[2] = quaternion[3] * factor;
+
+
+    // TODO:
+    // there are some special case need to pay attention
+    // 1. zero rotation
+    // 2. the theta should not be greater then pi.
 }
 
 template<class T>
@@ -26,12 +32,17 @@ void angle_axis_to_quaternion(const T* angle_axis, T* quaternion) {
     quaternion[3] = sine_norm_2 * angle_axis[2] / norm;
 
   } else {
+    // when the theta is closest to zero.
+    // then the sqrt of the theta will also be zero since the argument is zero.
+    // Thus we use the Taylor series to approximate and truncating at one term.
+    // the value and first derivatives will be computed correctly when Jets are
+    // used.
 
-      quaternion[0] = T(1.0);
-      T k = T(0.5);
-      quaternion[1] = angle_axis[0] * k;
-      quaternion[2] = angle_axis[1] * k;
-      quaternion[3] = angle_axis[2] * k;
+    quaternion[0] = T(1.0);
+    T k = T(0.5);
+    quaternion[1] = angle_axis[0] * k;
+    quaternion[2] = angle_axis[1] * k;
+    quaternion[3] = angle_axis[2] * k;
   }
 }
 
