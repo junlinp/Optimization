9136212b2a9789b78683e1a859567d6985e4f484
junlinp <junlinp@qq.com>
2022-09-25 19:34:09 +0800

    bazel build

--- bal/ceres_bal_solver.cc
+++ bal/ceres_bal_solver.cc
@@ -3,9 +3,38 @@
 #include "ceres/autodiff_cost_function.h"
 #include "ceres/problem.h"
 #include "ceres/solver.h"
+#include "ceres/rotation.h"
 
-#include "evaluate.h"
 #include "problem.h"
+
+// camera model see 
+// http://grail.cs.washington.edu/projects/bal/
+//
+struct ProjectFunction {
+    ProjectFunction(double u, double v) : u(u), v(v) {}
+    double u, v;
+    template<class T>
+    bool operator()(const T* camera_param,const T* point, T* residual) const {
+        T output_point[3];
+        ceres::AngleAxisRotatePoint(camera_param, point, output_point);
+        output_point[0] += camera_param[3];
+        output_point[1] += camera_param[4];
+        output_point[2] += camera_param[5];
+
+        output_point[0] /= -output_point[2];
+        output_point[1] /= -output_point[2];
+        T focal = camera_param[6];
+        T K1 = camera_param[7];
+        T K2 = camera_param[8];
+
+        T p_norm_2 = output_point[0] * output_point[0] + output_point[1] * output_point[1];
+        T distorsion = T(1.0) + K1 * p_norm_2 + K2 * p_norm_2 * p_norm_2;
+        residual[0] = T(u) - focal * distorsion * output_point[0];
+        residual[1] = T(v) - focal * distorsion * output_point[1];
+
+        return true;
+    }
+};
 
 void CeresProblemSolver::Solve(Problem &problem) {
     ceres::Problem pro;
---
+++ third_party/eigen_build.BUILD
@@ -0,0 +1,16 @@
+cc_library(
+	name = "eigen",
+	hdrs = glob([
+		"Eigen/*",
+		"Eigen/**/*.h",
+		"unsupported/Eigen/*",
+		"unsupported/Eigen/**/*.h",
+	]),
+	defines = [],
+	includes = ["."],
+	copts = ["-std=c++17"],
+	visibility = ["//visibility:public"]
+)
+
+
+
