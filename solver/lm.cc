8eea33f55b32bae9b3b8203bd6ce397cca04a189
junlinp <junlinp@deepmirror.com>
2023-08-01 12:17:47 +0800

    fix

--- solver/lm.cc
+++ solver/lm.cc
@@ -11,27 +11,18 @@ double QuadraticSolve(const Eigen::VectorXd& s, const Eigen::VectorXd& d, double
 }
 
 void TRSSolve(const Eigen::MatrixXd& J, const Eigen::VectorXd& e, double trust_region, Eigen::VectorXd* step) {
+    // TODO(junlinp): precondition
     // TODO(junlinp): B can be optimization
-    Eigen::VectorXd s = Eigen::VectorXd::Zero(e.cols());
-    std::cout << "s" << std::endl;
+    Eigen::VectorXd s = Eigen::VectorXd::Zero(e.rows());
     Eigen::VectorXd g = J.transpose() * e;
-    std::cout << "g" << std::endl;
     Eigen::MatrixXd B = J.transpose() * J;
-    std::cout << "B" << std::endl;
     Eigen::VectorXd d = -g;
-    std::cout << "d" << std::endl;
-    std::cout << "g row and col : " << g.rows() << ", " << g.cols() << std::endl;
-    double sum = 0.0;
-    for(int i = 0; i < g.rows(); i++) {
-        sum += g(i) * g(i);
-    }
-    std::cout << "sum : " << sum << std::endl;
+ 
     // Truncated CG
-    while(g.norm() < 1e-6) {
-        std::cout << "last_square_norm_" << std::endl;
+    while(g.norm() > 1e-6) {
         double last_square_norm_g = g.dot(g);
         double dT_B_d = d.dot(B * d);
-        if (dT_B_d< 0) {
+        if (dT_B_d < 0) {
             // alpha should be larger then 0
             double step_size = QuadraticSolve(s, d, trust_region);
             *step = s + step_size * d;
@@ -40,7 +31,7 @@ void TRSSolve(const Eigen::MatrixXd& J, const Eigen::VectorXd& e, double trust_r
         double alpha = last_square_norm_g / dT_B_d;
 
         Eigen::VectorXd new_s = s + alpha * d;
-        if (new_s.dot(new_s) > trust_region * trust_region) {
+        if (new_s.squaredNorm() > trust_region * trust_region) {
             double step_size = QuadraticSolve(s, d, trust_region);
             *step = s + step_size * d;
             return;
@@ -62,12 +53,12 @@ bool LMSolver::Solve(Eigen::VectorXd *x) {
     while(iterator++ < max_iteration) {
         Eigen::MatrixXd J = function_->Jacobians(x0);
         Eigen::VectorXd e = function_->Evaluate(x0);
-        std::cout << "Evaluate " << std::endl;
-        Eigen::VectorXd step;
+        std::cout << "Evaluate :" << 0.5 * e.dot(e) << std::endl;
+        Eigen::VectorXd step = Eigen::VectorXd::Zero(function_->VariableDimension());
         TRSSolve(J, e, trust_region, &step);
         // need function to evaluate the next step
         if (AcceptStepOrNot(x0, step, &trust_region)) {
-            x0 = x0 + step;
+          x0 = x0 + step;
         }
     }
     *x = x0;
@@ -77,10 +68,14 @@ bool LMSolver::Solve(Eigen::VectorXd *x) {
 bool LMSolver::AcceptStepOrNot(const Eigen::VectorXd &x, const Eigen::VectorXd &step, double *trust_region) {
     const double eta_v = 0.9;
     const double eta_s = 0.1;
-
+    std::cout << "AcceptStepOrNot" << std::endl;
     Eigen::VectorXd fx = function_->Evaluate(x);
-    Eigen::MatrixXd J = function_->Evaluate(x);
+    std::cout << "Evaluate fx" << std::endl;
+    Eigen::MatrixXd J = function_->Jacobians(x);
+    std::cout << "Evaluate J" << std::endl;
     Eigen::VectorXd fx_next = function_->Evaluate(x + step);
+    std::cout << "Evaluate fx_next" << std::endl;
+    std::cout << "fx_next row : " << fx_next.rows() << std::endl;
 
     double rho = (fx.dot(fx) - fx_next.dot(fx_next)) /
                  (fx.dot(fx) - (fx + J * step).squaredNorm());
@@ -94,5 +89,6 @@ bool LMSolver::AcceptStepOrNot(const Eigen::VectorXd &x, const Eigen::VectorXd &
         *trust_region *= 0.5;
         return false;
     }
+    return false;
 }
 
