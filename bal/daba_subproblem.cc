fcb685ac4fea2744857309b1c2aaef23854e4069
junlinp <junlinp@deepmirror.com>
2023-09-06 14:29:14 +0800

    refactor daba

--- bal/daba_subproblem.cc
+++ bal/daba_subproblem.cc
@@ -1,9 +1,11 @@
 #include "daba_subproblem.h"
-#include "cost_function_auto.h"
+
+#include <fstream>
 #include <mutex>
 #include <string>
-#include <fstream>
+
 #include "ceres/solver.h"
+#include "cost_function_auto.h"
 
 DabaSubproblem::DabaSubproblem(int cluster_id) : cluster_id_(cluster_id) {}
 
@@ -11,15 +13,18 @@ void DabaSubproblem::AddInternalEdge(
     int64_t camera_id, const std::array<double, 9> &camera_parameters,
     int64_t point_id, const std::array<double, 3> &point_parameters,
     std::array<double, 2> uv) {
-  internal_camera_[camera_id] = camera_parameters;
-  internal_point_[point_id] = point_parameters;
+  camera_parameters_[camera_id] = camera_parameters;
+  point_parameters_[point_id] = point_parameters;
+
   ceres::CostFunction *ray_cost_function =
       new ceres::AutoDiffCostFunction<RayCostFunction, 3, 9, 3>(
           new RayCostFunction(uv[0], uv[1]));
 
   problem_.AddResidualBlock(ray_cost_function, nullptr,
-                            internal_camera_[camera_id].data(),
-                            internal_point_[point_id].data());
+                            camera_parameters_[camera_id].data(),
+                            point_parameters_[point_id].data());
+
+  internal_costfunction_[{camera_id, point_id}] = ray_cost_function;
 }
 
 void DabaSubproblem::AddCamera(
@@ -27,7 +32,7 @@ void DabaSubproblem::AddCamera(
     int64_t external_point_id,
     const std::array<double, 3> &external_point_parameters,
     const std::array<double, 2> &uv) {
-  external_camera_[camera_id] = camera_parameters;
+  camera_parameters_[camera_id] = camera_parameters;
   previous_external_camera_[camera_id] = camera_parameters;
 
   // it should be update by ReceiveExternalPoint
@@ -40,41 +45,44 @@ void DabaSubproblem::AddCamera(
               external_other_point_[external_point_id].data(), uv[0], uv[1]));
 
   problem_.AddResidualBlock(camera_cost_function, nullptr,
-                            external_camera_[camera_id].data());
+                            camera_parameters_[camera_id].data());
+  camera_surrogate_costfunction_[camera_id] = camera_cost_function;
 }
 
-void DabaSubproblem::AddPoint(int64_t point_id, const std::array<double, 3> &point_parameters,
-              int64_t external_camera_id,
-              const std::array<double, 9> &external_camera_parameters,
-              const std::array<double, 2> &uv) {
-                external_point_[point_id] = point_parameters;
-                previous_external_point_[point_id] = point_parameters;
+void DabaSubproblem::AddPoint(
+    int64_t point_id, const std::array<double, 3> &point_parameters,
+    int64_t external_camera_id,
+    const std::array<double, 9> &external_camera_parameters,
+    const std::array<double, 2> &uv) {
+  point_parameters_[point_id] = point_parameters;
+  previous_external_point_[point_id] = point_parameters;
 
-                external_other_camera_[external_camera_id] = external_camera_parameters;
+  external_other_camera_[external_camera_id] = external_camera_parameters;
 
-                ceres::CostFunction *landmark_cost_function =
-                    new ceres::AutoDiffCostFunction<
-                        LandmarkSurrogatecostFunction, 3, 3>(
-                        new LandmarkSurrogatecostFunction(
-                            external_other_camera_[external_camera_id].data(),
-                            previous_external_point_[point_id].data(), uv[0],
-                            uv[1]));
+  ceres::CostFunction *landmark_cost_function =
+      new ceres::AutoDiffCostFunction<LandmarkSurrogatecostFunction, 3, 3>(
+          new LandmarkSurrogatecostFunction(
+              external_other_camera_.at(external_camera_id).data(),
+              previous_external_point_.at(point_id).data(), uv[0], uv[1]));
 
-                problem_.AddResidualBlock(landmark_cost_function, nullptr,
-                                          external_point_[point_id].data());
-}
-
-// should be thread-safe
-void DabaSubproblem::ReceiveExternalCamera(int64_t external_other_camera_id,
-                           const std::array<double, 9> &camera_parameters) {
- std::lock_guard<std::mutex> lk_(external_camera_queue_mutex_);
- external_other_camera_queue_.push(
-     std::make_pair(external_other_camera_id, camera_parameters));
+  problem_.AddResidualBlock(landmark_cost_function, nullptr,
+                            point_parameters_.at(point_id).data());
+  point_surrogate_costfunction_[point_id] = landmark_cost_function;
 }
 
 // should be thread-safe
-void DabaSubproblem::ReceiveExternalPoint(int64_t external_other_point_id,
-                          const std::array<double, 3> &point_parameters) {
+void DabaSubproblem::ReceiveExternalCamera(
+    int64_t external_other_camera_id,
+    const std::array<double, 9> &camera_parameters) {
+  std::lock_guard<std::mutex> lk_(external_camera_queue_mutex_);
+  external_other_camera_queue_.push(
+      std::make_pair(external_other_camera_id, camera_parameters));
+}
+
+// should be thread-safe
+void DabaSubproblem::ReceiveExternalPoint(
+    int64_t external_other_point_id,
+    const std::array<double, 3> &point_parameters) {
   std::lock_guard<std::mutex> lk_k(external_point_queue_mutex_);
   external_other_point_queue_.push(
       std::make_pair(external_other_point_id, point_parameters));
@@ -82,11 +90,11 @@ void DabaSubproblem::ReceiveExternalPoint(int64_t external_other_point_id,
 
 void DabaSubproblem::Start() {
   auto solve_functor = [this]() {
-    constexpr int max_iteration = 1;
+    constexpr int max_iteration = 32;
     int iteration = 0;
     std::ofstream ofs(std::to_string(cluster_id_) + "loss.txt");
 
-    while(iteration++ < max_iteration) {
+    while (iteration++ < max_iteration) {
       {
         std::lock_guard<std::mutex> lk_(external_camera_queue_mutex_);
         while (!external_other_camera_queue_.empty()) {
@@ -109,37 +117,36 @@ void DabaSubproblem::Start() {
 
       // solve
 
+      std::cout << "cluster " << cluster_id_ << " start solve" << std::endl;
       ceres::Solver::Summary summary;
       ceres::Solver::Options options;
-      options.max_num_iterations = 1024;
+      options.max_num_iterations = 16;
       ceres::Solve(options, &problem_, &summary);
 
       std::cout << "cluster " << cluster_id_ << " iteration " << iteration
                 << " summary : " << summary.BriefReport() << std::endl;
-        ofs << summary.BriefReport() << std::endl;
+      ofs << summary.BriefReport() << std::endl;
 
-      for (auto& [camera_id, parameters]: external_camera_) {
-        std::copy(parameters.begin(), parameters.end(),
-                  previous_external_camera_[camera_id].begin());
+      for (auto &[camera_id, parameters] : previous_external_camera_) {
+        std::copy(camera_parameters_.at(camera_id).begin(),
+                  camera_parameters_.at(camera_id).end(), parameters.begin());
       }
 
-      for (auto& [point_id, parameters] : external_point_) {
-        std::copy(parameters.begin(), parameters.end(),
-                  previous_external_point_[point_id].begin());
+      for (auto &[point_id, parameters] : previous_external_point_) {
+        std::copy(point_parameters_.at(point_id).begin(),
+                  point_parameters_.at(point_id).end(), parameters.begin());
       }
 
-      for (auto& [camera_id, parameters] : external_camera_) {
-        boardcast_camera_callback_(camera_id, parameters);
+      for (auto &[camera_id, parameters] : previous_external_camera_) {
+        boardcast_camera_callback_(camera_id, camera_parameters_.at(camera_id));
       }
 
-      for (auto& [point_id, parameters] : external_point_) {
-        boardcast_point_callback_(point_id, parameters);
+      for (auto &[point_id, parameters] : previous_external_point_) {
+        boardcast_point_callback_(point_id, point_parameters_.at(point_id));
       }
     }
     ofs.close();
   };
   thread_ = std::thread(solve_functor);
 }
-void DabaSubproblem::WaitForFinish() {
-    thread_.join();
-}
+void DabaSubproblem::WaitForFinish() { thread_.join(); }
--- bal/daba_subproblem.h
+++ bal/daba_subproblem.h
@@ -1,10 +1,12 @@
 #ifndef BAL_DATA_SUBPROBLEM_H_
 #define BAL_DATA_SUBPROBLEM_H_
 
+#include <ceres/cost_function.h>
 #include <tuple>
 #include <map>
 #include <thread>
 #include <queue>
+#include <mutex>
 
 #include "bal/problem.h"
 #include "ceres/problem.h"
@@ -54,40 +56,42 @@ public:
   void WaitForFinish();
 
   std::map<int64_t, CameraParameters> ClusterCameraData() const {
-    std::map<int64_t, CameraParameters>  res = internal_camera_;
-    
-    for (auto piar : external_camera_) {
-        res.emplace(piar);
-    }
-    return res;
+    return camera_parameters_;
   }
 
   std::map<int64_t, PointParameters> ClusterPointData() const {
-    std::map<int64_t, PointParameters> res = internal_point_;
-
-    for (auto pair : external_point_) {
-        res.emplace(pair);
-    }
-    return res;
+    return point_parameters_;
   }
 
-private:
+
+  auto InternalCostFunction() { return internal_costfunction_;}
+  auto CameraCostFunction() { return camera_surrogate_costfunction_;}
+  auto PointCostFunction() { return point_surrogate_costfunction_; }
+
+  auto ExternalOtherCamera() { return external_other_camera_;}
+  auto ExternalOtherPoint() { return external_other_point_; }
+
+ private:
   // camera id,  point id, uv
 
-  std::map<int64_t, CameraParameters> internal_camera_;
-  std::map<int64_t, PointParameters> internal_point_;
+  std::map<int64_t, CameraParameters> camera_parameters_;
+  std::map<int64_t, PointParameters> point_parameters_;
 
-  std::map<int64_t, CameraParameters> external_camera_;
   std::map<int64_t, CameraParameters> previous_external_camera_;
-  std::map<int64_t, PointParameters> external_point_;
   std::map<int64_t, PointParameters> previous_external_point_;
 
   std::map<int64_t, CameraParameters> external_other_camera_;
   std::map<int64_t, PointParameters> external_other_point_;
+
   std::queue<std::pair<int64_t, CameraParameters>> external_other_camera_queue_;
   std::mutex external_camera_queue_mutex_;
   std::queue<std::pair<int64_t, PointParameters>> external_other_point_queue_;
   std::mutex external_point_queue_mutex_;
+
+  
+  std::map<std::pair<int64_t, int64_t>, ceres::CostFunction*> internal_costfunction_;
+  std::map<int64_t, ceres::CostFunction*> camera_surrogate_costfunction_;
+  std::map<int64_t, ceres::CostFunction*> point_surrogate_costfunction_;
 
   std::function<void(int64_t, CameraParameters)> boardcast_camera_callback_;
   std::function<void(int64_t, PointParameters)> boardcast_point_callback_;
