6e75ebcdfa15faee98e8c00cb6699d5052f842aa
junlinp <junlinp@deepmirror.com>
2023-09-20 11:53:15 +0800

    manifold and manifold unittest

--- RGD/manifold.h
+++ RGD/manifold.h
@@ -1,9 +1,11 @@
 #ifndef RGD_MANIFOLD_H_
 #define RGD_MANIFOLD_H_
 #include <Eigen/Dense>
+#include <Eigen/src/Core/Matrix.h>
 
-template <int ambient_space_size, int tangent_space_size> class Manifold {
-public:
+template <int ambient_space_size, int tangent_space_size>
+class Manifold {
+ public:
   constexpr static int AmbientSpaceSize = ambient_space_size;
   constexpr static int TangentSpaceSize = tangent_space_size;
   using AmbientSpaceVector = Eigen::Matrix<double, ambient_space_size, 1>;
@@ -16,34 +18,127 @@ public:
   // f is a real value function for manifold.
   // so we project the general gradient of f to the tangent sapce of manifold at
   // point x
-  static TangentSpaceVector
-  Project(const Manifold &x, const GeneralJacobianVector &general_gradient);
+  static TangentSpaceVector Project(
+      const Manifold &x, const GeneralJacobianVector &general_gradient);
 };
 
 template <int ambient_space_size, int tangent_space_size>
-class EuclideanManifold {};
+class EuclideanManifold {
+ public:
+  constexpr static int AmbientSpaceSize = ambient_space_size;
+  constexpr static int TangentSpaceSize = tangent_space_size;
+  using AmbientSpaceVector = Eigen::Matrix<double, ambient_space_size, 1>;
+  using TangentSpaceVector = Eigen::Matrix<double, tangent_space_size, 1>;
+  using GeneralJacobianVector = Eigen::Matrix<double, ambient_space_size, 1>;
 
-class RotationMatrixManifold : public Manifold<9, 9> {
-public:
+  EuclideanManifold() : vector_() { vector_.setZero(); }
+  EuclideanManifold(const AmbientSpaceVector &vector) : vector_(vector) {}
+  AmbientSpaceVector AmbientVector() const { return vector_; }
+
   // y = Retraction(x + v)
-  AmbientSpaceVector Retraction(const AmbientSpaceVector &x,
-                                const TangentSpaceVector &v) override;
+  static EuclideanManifold Retraction(const EuclideanManifold &x,
+                                      const TangentSpaceVector &v) {
+    return EuclideanManifold(x.vector_ + v);
+  }
 
   // f is a real value function for manifold.
   // so we project the general gradient of f to the tangent sapce of manifold at
   // point x
-  TangentSpaceVector
-  Project(const AmbientSpaceVector &x,
-          const GeneralJacobianVector &general_gradient) override;
+  static TangentSpaceVector Project(
+      const EuclideanManifold & /*x*/,
+      const GeneralJacobianVector &general_gradient) {
+    return general_gradient;
+  }
 
-private:
+ private:
   AmbientSpaceVector vector_;
 };
 
-template <class Manifold_lhs, class Manifold_rhs> class ProductManifold {
+class RotationMatrixManifold : {
 public:
-  constexpr static int AmbientSpaceSize = Manifold_lhs::AmbientSpaceSize + Manifold_rhs::AmbientSpaceSize;
-  constexpr static int TangentSpaceSize = Manifold_lhs::TangentSpaceSize + Manifold_rhs::TangentSpaceSize;
+  constexpr static int AmbientSpaceSize = 9;
+  constexpr static int TangentSpaceSize = 9;
+  using AmbientSpaceVector = Eigen::Matrix<double, AmbientSpaceSize, 1>;
+  using TangentSpaceVector = Eigen::Matrix<double, TangentSpaceSize, 1>;
+  using GeneralJacobianVector = Eigen::Matrix<double, AmbientSpaceSize, 1>;
+
+  RotationMatrixManifold() : vector_() {
+    vector_ << 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0;
+  }
+  RotationMatrixManifold(const AmbientSpaceVector &vector) : vector_(vector) {}
+  AmbientSpaceVector AmbientVector() const { return vector_; }
+
+  // y = Retraction(x + v)
+  static AmbientSpaceVector Retraction(const AmbientSpaceVector &x,
+                                const TangentSpaceVector &v) {
+    return QRDecomposition(x, v);
+  }
+
+  // f is a real value function for manifold.
+  // so we project the general gradient of f to the tangent sapce of manifold
+  // at point x
+  static TangentSpaceVector Project(
+      const AmbientSpaceVector &x,
+      const GeneralJacobianVector &general_gradient) {
+
+    Eigen::Map<const Eigen::Matrix3d> X(x.data());
+    Eigen::Map<const Eigen::Matrix3d> V(general_gradient.data());
+
+    auto SkewPart = [](const Eigen::Matrix3d &X) -> Eigen::Matrix3d {
+      return 0.5 * (X - X.transpose());
+    };
+
+    Eigen::Matrix3d XTU = X.transpose() * V;
+
+    Eigen::Matrix3d Q = X * SkewPart(XTU);
+    return Eigen::Map<TangentSpaceVector>(Q.data());
+  }
+
+ private:
+   
+  static TangentSpaceVector CayleyTransformation(const AmbientSpaceVector&x, const TangentSpaceVector& v) {
+    Eigen::Map<const Eigen::Matrix3d> X(x.data());
+    Eigen::Map<const Eigen::Matrix3d> V(v.data());
+    Eigen::Matrix3d Identity = Eigen::Matrix3d::Identity();
+    Eigen::Matrix3d W =
+        (Identity - 0.5 * X * X.transpose()) * V * X.transpose() -
+        X * V.transpose() * (Identity - 0.5 * X * X.transpose());
+    
+    Eigen::Matrix3d Q = (Identity - 0.5 * W).inverse() * (Identity + 0.5 * W) * X;
+    return Eigen::Map<TangentSpaceVector>(Q.data());
+  }
+
+  static TangentSpaceVector QRDecomposition(const AmbientSpaceVector&x, const TangentSpaceVector& v) {
+    Eigen::Map<const Eigen::Matrix3d> X(x.data());
+    Eigen::Map<const Eigen::Matrix3d> V(v.data());
+
+    auto QR_solver = (X + V).householderQr();
+    Eigen::Matrix3d Q = QR_solver.householderQ();
+    Eigen::Matrix3d R = Q.transpose() * (X + V);
+    Eigen::Matrix3d identity = Eigen::Matrix3d::Identity();
+    if (Q.determinant() < 0.0) {
+      for (int i = 0; i < 3; i++) {
+        if (R(i, i) < 0.0) {
+          identity(i, i) = -1.0;
+        }
+      }
+    }
+    Q = Q * identity;
+    return Eigen::Map<TangentSpaceVector>(Q.data());
+  }
+  
+
+  AmbientSpaceVector vector_;
+};
+
+template <class Manifold_lhs, class Manifold_rhs>
+class ProductManifold {
+ public:
+  constexpr static int AmbientSpaceSize =
+      Manifold_lhs::AmbientSpaceSize + Manifold_rhs::AmbientSpaceSize;
+  constexpr static int TangentSpaceSize =
+      Manifold_lhs::TangentSpaceSize + Manifold_rhs::TangentSpaceSize;
+
   using AmbientSpaceVector = Eigen::Matrix<double, AmbientSpaceSize, 1>;
   using TangentSpaceVector = Eigen::Matrix<double, TangentSpaceSize, 1>;
   using GeneralJacobianVector = Eigen::Matrix<double, AmbientSpaceSize, 1>;
@@ -53,19 +148,31 @@ public:
       : vector_(ambient_vector) {}
 
   ProductManifold(const Manifold_lhs &manifold_lhs,
-                  const Manifold_rhs &manifold_rhs)
-      : {
-    vector_.head<Manifold_lhs::AmbientSpaceSize> = manifold_lhs.AmbientVector();
-    vector_.tail<Manifold_rhs::AmbientSpaceSize> = manifold_rhs.AmbientVector();
+                  const Manifold_rhs &manifold_rhs) {
+    constexpr int lhs_ambient_space_size = Manifold_lhs::AmbientSpaceSize;
+    constexpr int rhs_ambient_space_size = Manifold_rhs::AmbientSpaceSize;
+
+    vector_.block(0, 0, lhs_ambient_space_size, 1) =
+        manifold_lhs.AmbientVector();
+    vector_.block(Manifold_lhs::AmbientSpaceSize, 0, rhs_ambient_space_size,
+                  1) = manifold_rhs.AmbientVector();
   }
+
+  AmbientSpaceVector AmbientVector() const { return vector_; }
 
   // y = Retraction(x + v)
   static ProductManifold Retraction(const ProductManifold &x,
                                     const TangentSpaceVector &v) {
-    Manifold_lhs::TangentSpaceVector lhs_tangent = v.head<Manifold_lhs::TangentSpaceSize>();
-    Manifold_rhs::TangentSpaceVector rhs_tangent = v.tail<Manifold_rhs::TangentSpacesize>();
-    Manifold_lhs res_lhs = Manifold_lhs::Retraction(x.manifold_first, lhs_tangent);
-    Manifold_rhs res_rhs = Manifold_rhs::Retraction(x.manifold_second, rhs_tangent);
+    typename Manifold_lhs::TangentSpaceVector lhs_tangent =
+        v.block(0, 0, Manifold_lhs::TangentSpaceSize, 1);
+
+    typename Manifold_rhs::TangentSpaceVector rhs_tangent = v.block(
+        Manifold_lhs::TangentSpaceSize, 0, Manifold_rhs::TangentSpaceSize, 1);
+
+    Manifold_lhs res_lhs =
+        Manifold_lhs::Retraction(x.FirstPartAmbientVector(), lhs_tangent);
+    Manifold_rhs res_rhs =
+        Manifold_rhs::Retraction(x.SecondPartAmbientVector(), rhs_tangent);
 
     return ProductManifold(res_lhs, res_rhs);
   }
@@ -73,27 +180,48 @@ public:
   // f is a real value function for manifold.
   // so we project the general gradient of f to the tangent sapce of manifold at
   // point x
-  static TangentSpaceVector
-  Project(const ProductManifold &x,
-          const GeneralJacobianVector &general_gradient) {
-          }
+  static TangentSpaceVector Project(
+      const ProductManifold &x, const GeneralJacobianVector &general_gradient) {
+    TangentSpaceVector res;
+    res.block(0, 0, Manifold_lhs::TangentSpaceSize, 1) = Manifold_lhs::Project(
+        x.FirstPartAmbientVector(),
+        general_gradient.block(0, 0, Manifold_lhs::TangentSpaceSize, 1));
 
-private:
-Manifold_lhs manifold_first;
-Manifold_rhs manifold_second;
+    res.block(Manifold_lhs::TangentSpaceSize, 0, Manifold_rhs::TangentSpaceSize,
+              1) =
+        Manifold_rhs::Project(
+            x.SecondPartAmbientVector(),
+            general_gradient.block(Manifold_lhs::TangentSpaceSize, 0,
+                                   Manifold_rhs::TangentSpaceSize, 1));
+    return res;
+  }
+
+ private:
+  auto FirstPartAmbientVector() const {
+    return Manifold_lhs(vector_.block(0, 0, Manifold_lhs::AmbientSpaceSize, 1));
+  }
+
+  auto SecondPartAmbientVector() const {
+    return Manifold_rhs(vector_.block(Manifold_lhs::AmbientSpaceSize, 0,
+                                      Manifold_rhs::AmbientSpaceSize, 1));
+  }
+
+  AmbientSpaceVector vector_;
 };
 
-template<class T1, class T2>
-auto CreateManifold(T1&& manifold_lhs, T2&&manifold_rhs) {
-  return ProductManifold<T1, T2>(manifold_lhs, manifold_rhs);
+template <class T>
+using remove_cvrf_t = std::remove_cv_t<std::remove_reference_t<T>>;
+
+template <class T1, class T2>
+auto CreateManifold(T1 &&manifold_lhs, T2 &&manifold_rhs) {
+  return ProductManifold<remove_cvrf_t<T1>, remove_cvrf_t<T2>>(manifold_lhs,
+                                                               manifold_rhs);
 }
 
-template<class T, class... ARGS>
-auto CreateManifold(T&& manifold, ARGS... manifolds) {
-    auto rhs = CreateManifold(manifolds...);
-    return ProductManifold(manifold, rhs);
-};
+template <class T, class... ARGS>
+auto CreateManifold(T &&manifold, ARGS... manifolds) {
+  auto rhs = CreateManifold(manifolds...);
+  return ProductManifold<remove_cvrf_t<T>, decltype(rhs)>(manifold, rhs);
+}
 
-
-
-#endif // RGD_MANIFOLD_H_
+#endif  // RGD_MANIFOLD_H_
--- RGD/rgd.h
+++ RGD/rgd.h
@@ -2,37 +2,59 @@
 #define RGB_RGD_H_
 #include "manifold.h"
 #include "so3_cost_function_interface.h"
+#include <type_traits>
 
 bool rgd(const SO3CostFunctionInterface &cost_function,
          std::vector<SO3Manifold::Vector> *x_init);
 
-template <int Manifold, int residual_dimension> class CostFunctionFunctor {
-public:
-  using ResidualVector = Eigen::Matrix<double, residual_dimension, 1>;
+// template <int Manifold, int residual_dimension> 
+// class CostFunctionFunctor {
+// public:
+//   using ResidualVector = Eigen::Matrix<double, residual_dimension, 1>;
 
-  using JacobianMatrix =
-      Eigen::Matrix<double, residual_dimension, Manifold::AmbientSpaceSize>;
+//   using JacobianMatrix =
+//       Eigen::Matrix<double, residual_dimension, Manifold::AmbientSpaceSize>;
 
-  bool Evaluate(const Manifold *parameters, ResidualVector *residuals,
-                JacobianMatrix *) const;
-};
+//   bool Evaluate(const Manifold& parameters, ResidualVector *residuals,
+//                 JacobianMatrix *) const;
+// };
 
 template <class CostFunctionFunctor, class Manifold>
-bool LeastQuaresRiemannGredientDescent(
+bool LeastQuaresRiemannGredientDescentLinearSearch(
     CostFunctionFunctor &&costfunction_functor,
-    Manifold &&manifold_init_value) {
-  typename CostFunctionFunctor::ResidualVector residuals;
-  typename CostFunctionFunctor::JacobianMatrix jacobians;
-  costfunction_functor.Evaluate(&manifold_init_value, &residuals, &jacobians);
+    Manifold &&manifold_init_value, typename std::remove_reference_t<Manifold>::TangentSpaceVector& step) {
+      using OriginManifold = std::remove_reference_t<std::remove_cv_t<Manifold>>;
+
+  typename std::remove_reference_t<CostFunctionFunctor>::ResidualVector residuals;
+  typename std::remove_reference_t<CostFunctionFunctor>::JacobianMatrix jacobians;
+  costfunction_functor.Evaluate(manifold_init_value, &residuals, &jacobians);
   
   double least_quares_error = 0.5 * residuals.squaredNorm();
-  Eigen::VectorXd general_gradient = residuals.transpose() * jacobians;
+  Eigen::VectorXd general_gradient = jacobians.transpose() * residuals;
 
-  typename Manifold::TangentSpaceVector tangent_gradient =
-      Manifold::Project(manifold_init_value, general_gradient);
+  typename OriginManifold::TangentSpaceVector tangent_gradient =
+      OriginManifold::Project(manifold_init_value, general_gradient);
 
-  // linear search
-  // return Step
+  int max_iteration = 1024;
+  int iteration = 0;
+  double alpha = 1.0;
+  double beta = 0.8;
+  double eta = 1e-4;
+  
+  double gradient_norm =  tangent_gradient.squaredNorm();
+  while(iteration++ < max_iteration) {
+    OriginManifold next_step = OriginManifold::Retraction(manifold_init_value, alpha * tangent_gradient);
+    costfunction_functor.Evaluate(next_step, &residuals, nullptr);
+    double next_step_least_quares_error = 0.5 * residuals.squaredNorm();
+
+    if ((least_quares_error - next_step_least_quares_error) < eta * alpha * gradient_norm) {
+      step = alpha * tangent_gradient;
+      return true;
+    } else {
+      alpha *= beta;
+    }
+  }
+  return false;
 }
 
 #endif //  RGB_RGD_H_
